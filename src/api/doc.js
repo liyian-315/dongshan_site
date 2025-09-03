b5159b808c68c8f7e6d2518b48854c7fb9eb4eab
liyian-315 <859679381@qq.com>
2025-09-03 16:20:24 +0800

    feat:镜像文档左侧目录改为三级

--- src/api/doc.js
+++ src/api/doc.js
@@ -1,5 +1,9 @@
-import { get } from './index'
+import {get} from './index'
 
 export async function fetchDocList(params) {
-  return get('/api/docs', params)
+    return get('/api/docs', params)
+}
+
+export async function fetchMenuList() {
+    return get('/api/docs_menu')
 }
